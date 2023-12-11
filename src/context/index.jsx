8d6035f28088bf0acafee06e20ac51c76ae2faa4
Ronit <ronitvirwani1@gmail.com>
2023-12-11 17:06:09 +0530

    deploy

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -7,7 +7,7 @@ import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  const { contract } = useContract('0x214930Eb11dcb02520d86E4F62964E43fBBAD7e8');
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
   const address = useAddress();
